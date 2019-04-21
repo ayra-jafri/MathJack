e130a6458db5731a1cfc03147c6bfea478771149
ayra-jafri <34120837+ayra-jafri@users.noreply.github.com>
2019-04-20 22:41:12 -0700

    updated the card game
    finished

---
+++ mathGUIRunner.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class that plays the GUI version of the Thirteens game.
+ * See accompanying documents for a description of how Thirteens is played.
+ */
+public class mathGUIRunner {
+	/**
+	 * Plays the GUI version of Thirteens.
+	 * @param args is not used.
+	 */
+	public static void main(String[] args) {
+		Board board = new mathBoard();
+		CardGameGUI gui = new CardGameGUI(board);
+		gui.displayGame();
+	}
+}
