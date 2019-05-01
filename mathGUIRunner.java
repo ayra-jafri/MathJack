4bdffbbf4135ebbae9b6e7ebd5358462fd498994
Ayra Jafri <34120837+ayra-jafri@users.noreply.github.com>
2019-05-01 10:27:00 -0700

    Update mathGUIRunner.java

--- mathGUIRunner.java
+++ mathGUIRunner.java
@@ -1,10 +1,10 @@
 /**
- * This is a class that plays the GUI version of the Thirteens game.
- * See accompanying documents for a description of how Thirteens is played.
+ * This is a class that plays the GUI version of the MathJack game.
+ * See accompanying documents for a description of how MathJack is played.
  */
 public class mathGUIRunner {
 	/**
-	 * Plays the GUI version of Thirteens.
+	 * Plays the GUI version of MathJack.
 	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
